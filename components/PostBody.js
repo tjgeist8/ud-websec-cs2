e571b6624f3484d84bce86ba53c1002b224f7718
tjgeist8 <tj.geist8@gmail.com>
2026-09-26 14:12:11 +0000

    Vulnerability removed. Ready for deployment

--- components/PostBody.js
+++ components/PostBody.js
@@ -1,5 +1,13 @@
-"use client";
+import sanitizeHtml from "sanitize-html";
+
+const postHtmlOptions = {
+  allowedTags: ["h1", "h2", "h3", "strong", "em", "code", "a", "li", "ul", "br"],
+  allowedAttributes: { a: ["href"] },
+  allowedSchemes: ["http", "https", "mailto"],
+  allowProtocolRelative: false
+};
 
 export default function PostBody({ html }) {
-  return <div className="post-body" dangerouslySetInnerHTML={{ __html: html }} />;
+  const safeHtml = sanitizeHtml(String(html ?? ""), postHtmlOptions);
+  return <div className="post-body" dangerouslySetInnerHTML={{ __html: safeHtml }} />;
 }
