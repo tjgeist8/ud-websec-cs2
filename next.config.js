99239c4b501eef6e9997aae0e3eda7f5a553d25b
tjgeist8 <tj.geist8@gmail.com>
2026-09-26 13:37:18 +0000

    Fixed a small error when running through the github codespace

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  serverExternalPackages: []
+  serverExternalPackages: [],
+  ...(process.env.NODE_ENV === "development" && {
+    experimental: {
+      serverActions: {
+        allowedOrigins: ["localhost:3000"]
+      }
+    }
+  })
 };
 
 module.exports = nextConfig;
